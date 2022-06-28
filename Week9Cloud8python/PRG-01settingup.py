f492762ca0385cae5001788ce67a25281317243d
Rolfsijtema <104761418+Rolfsijtema@users.noreply.github.com>
2022-06-28 09:17:27 +0200

    Python

---
+++ Week9Cloud8python/PRG-01settingup.py
@@ -0,0 +1,9 @@
+print("hello World!")
+
+
+Example path:
+rolf@MBP-van-Rolf cloud8-Rolfsijtema % /usr/local/bin/python
+3 /Users/rolf/Documents/GitHub/cloud8-Rolfsijtema/Week9Cloud
+8python/PRG-01settingup.py
+hello World!
+rolf@MBP-van-Rolf cloud8-Rolfsijtema % 
